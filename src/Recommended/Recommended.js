33ecbc92475085d6e8e01def015f4137cbe19e1d
tuzgbach <tungbach.tts@gmail.com>
2023-10-24 11:14:03 +0700

    first commit

---
+++ src/Recommended/Recommended.js
@@ -0,0 +1,25 @@
+import Button from "../components/Buttons";
+import "./Recommended.css";
+
+const Recommended = ({ handleClick }) => {
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Sản Phẩm của chúng tôi</h2>
+        <div className="recommended-flex">
+          <Button
+            onClickHandler={handleClick}
+            value=""
+            title="Tất cả sản phẩm"
+          />
+          <Button onClickHandler={handleClick} value="Nike" title="Nike" />
+          <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
+          <Button onClickHandler={handleClick} value="Puma" title="Puma" />
+          <Button onClickHandler={handleClick} value="Vans" title="Vans" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Recommended;
---
+++ src/Sidebar/Category/Category.js
@@ -0,0 +1,43 @@
+import "./Category.css";
+import Input from "../../components/Input";
+
+function Category({ handleChange }) {
+  return (
+    <div>
+      <h2 className="sidebar-title">Danh Mục Sản Phẩm</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className="checkmark"></span>Tất cả sản phẩm
+        </label>
+        <Input
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="test"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Category;
